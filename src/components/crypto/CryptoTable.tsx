821fe157b0f1c8761087a5e8802aacf056ec7431
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2022-03-10 13:12:32 +0100

    add typing to dummy data

--- src/components/crypto/CryptoTable.tsx
+++ src/components/crypto/CryptoTable.tsx
@@ -3,54 +3,58 @@ import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons';
 import { useMemo } from 'react';
 import { useTable, useSortBy } from 'react-table';
 
-const DUMMY_DATA = [
+const DUMMY_DATA: {
+  cryptoName: string;
+  priceInUSD: number;
+  marketCap: number;
+}[] = [
   {
-    crpytoName: 'Bitcoin',
+    cryptoName: 'Bitcoin',
     priceInUSD: 39297.43,
     marketCap: 743927748657,
   },
   {
-    crpytoName: 'Ethereum',
+    cryptoName: 'Ethereum',
     priceInUSD: 2604.5,
     marketCap: 311603507595,
   },
   {
-    crpytoName: 'BNB',
+    cryptoName: 'BNB',
     priceInUSD: 377.58,
     marketCap: 63389911412,
   },
   {
-    crpytoName: 'XRP',
+    cryptoName: 'XRP',
     priceInUSD: 0.734506,
     marketCap: 3510309186,
   },
   {
-    crpytoName: 'Terra',
+    cryptoName: 'Terra',
     priceInUSD: 97.2,
     marketCap: 34663679968,
   },
   {
-    crpytoName: 'Solana',
+    cryptoName: 'Solana',
     priceInUSD: 82.84,
     marketCap: 26694367656,
   },
   {
-    crpytoName: 'Cardano',
+    cryptoName: 'Cardano',
     priceInUSD: 0.80927,
     marketCap: 25926918976,
   },
   {
-    crpytoName: 'Avalanche',
+    cryptoName: 'Avalanche',
     priceInUSD: 73.37,
     marketCap: 19438799990,
   },
   {
-    crpytoName: 'Polkadot',
+    cryptoName: 'Polkadot',
     priceInUSD: 17.04,
     marketCap: 18545718814,
   },
   {
-    crpytoName: 'Dogecoin',
+    cryptoName: 'Dogecoin',
     priceInUSD: 0.116835,
     marketCap: 15448543380,
   },
