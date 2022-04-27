2b9f0a535972f6033ef7b2ee86f7de9bcfb851e7
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2022-04-27 12:59:20 +0200

    update AllCryptoTable

--- src/components/features/cryptocurrencies/AllCryptoTable/AllCryptoTable.tsx
+++ src/components/features/cryptocurrencies/AllCryptoTable/AllCryptoTable.tsx
@@ -2,30 +2,15 @@ import { Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
 import { useCrypto } from '../../../../../hooks/useCrypto';
 import Image from 'next/image';
 import { Coin } from '../../../../../types/crypto';
-import { Icon } from '@chakra-ui/react';
 import { BsStarFill, BsStar } from 'react-icons/bs';
-import { useState } from 'react';
 
 export const AllCryptoTable = () => {
   const { data: cryptocurrencies, error } = useCrypto();
-  const [favorites, setFavorites] = useState([]);
-
-  // const addFavorite = (coin: Coin, index: number) => {
-  //   let array = favorites;
-  //   let addArray = true;
-  //   array.map((coins: Coin[], key: number) => {
-  //     if (coins[index] === key) {
-  //       array.splice(key, 1);
-  //       addArray = false;
-  //     }
-  //   });
-  // };
 
   return (
     <Table>
       <Thead>
         <Tr>
-          <Th></Th>
           <Th>#</Th>
           <Th>Logo</Th>
           <Th>Name</Th>
@@ -40,26 +25,6 @@ export const AllCryptoTable = () => {
         {cryptocurrencies?.map((coin: Coin, index: string) => {
           return (
             <Tr key={coin.id}>
-              <Td>
-                {favorites ? (
-                  <Icon
-                    color="#ffcd3c"
-                    as={BsStarFill}
-                    w={6}
-                    h={6}
-                    _hover={{ cursor: 'pointer' }}
-                    // onClick={addFavorite(coin, index)}
-                  />
-                ) : (
-                  <Icon
-                    as={BsStar}
-                    w={6}
-                    h={6}
-                    _hover={{ cursor: 'pointer' }}
-                    // onClick={addFavorite(coin, index)}
-                  />
-                )}
-              </Td>
               <Td>{index + 1}</Td>
               <Td>
                 <Image
@@ -73,7 +38,7 @@ export const AllCryptoTable = () => {
               </Td>
               <Td>{coin.name}</Td>
               <Td>{coin.symbol.toUpperCase()}</Td>
-              <Td>
+              <Td fontStyle="bold">
                 $
                 {coin.current_price > 1
                   ? coin.current_price.toLocaleString(undefined, {
