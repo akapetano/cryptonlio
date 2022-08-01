de79ca7d5e24349809f9839e93959b89e73e84f6
Andrija Kapetanovic <andrija.kapetanovic@gmail.com>
2022-08-01 22:28:56 +0200

    add topTenCoins to useCrypto hook

--- hooks/useCrypto.ts
+++ hooks/useCrypto.ts
@@ -1,9 +1,11 @@
-import useSWR from 'swr';
-import { cryptoFetcher } from '../src/fetchers/cryptoFetcher';
-import { COINS_COINGECKO_API_URL } from '../src/fetchers/cryptoFetcher';
+import useSWR from "swr";
+import { cryptoFetcher } from "../src/fetchers/cryptoFetcher";
+import { COINS_COINGECKO_API_URL } from "../src/fetchers/cryptoFetcher";
 
 export const useCrypto = () => {
   const { data, error } = useSWR(COINS_COINGECKO_API_URL, cryptoFetcher);
 
-  return { data, isLoading: !error && !data, isError: error };
+  const topTenCoins = data?.slice(0, 10);
+
+  return { data, topTenCoins, isLoading: !error && !data, isError: error };
 };
