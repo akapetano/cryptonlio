f8e08ef982c944bc33e67ef323a76b339644844c
Andrija Kapetanovic <andrija.kapetanovic@gmail.com>
2022-09-19 22:52:21 +0200

    create separate type file for auth

---
+++ types/auth.ts
@@ -0,0 +1,8 @@
+export interface ISignUpFormValues {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+  ageConfirmation: boolean;
+}
