6f9de5fa2e867cb610ccd47d7c86bf68c160b9e6
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2022-10-18 23:53:45 +0200

    add optional chaining to the CoinDetails page on readMore

--- src/components/features/cryptocurrencies/[coin]/CoinDetails/CoinDetails.tsx
+++ src/components/features/cryptocurrencies/[coin]/CoinDetails/CoinDetails.tsx
@@ -9,20 +9,20 @@ import {
   Button,
   Badge,
   Icon,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 import parse, {
   attributesToProps,
   Element,
   Text as TextType,
-} from 'html-react-parser';
-import Image from 'next/image';
-import { useState } from 'react';
-import { CoinById } from '../../../../../../types/crypto';
-import { ChartSelector } from '../charts/ChartSelector/ChartSelector';
-import { CoinMarketData } from './CoinMarketData/MarketData';
-import { CoinDescription } from './CoinDescription/CoinDescription';
-import { CoinResources } from './CoinResources/CoinResources';
-import { CoinHeader } from './CoinHeader/CoinHeader';
+} from "html-react-parser";
+import Image from "next/image";
+import { useState } from "react";
+import { CoinById } from "../../../../../../types/crypto";
+import { ChartSelector } from "../charts/ChartSelector/ChartSelector";
+import { CoinMarketData } from "./CoinMarketData/MarketData";
+import { CoinDescription } from "./CoinDescription/CoinDescription";
+import { CoinResources } from "./CoinResources/CoinResources";
+import { CoinHeader } from "./CoinHeader/CoinHeader";
 
 interface ICoinDetails {
   coin: CoinById;
@@ -32,10 +32,10 @@ export const CoinDetails = ({ coin }: ICoinDetails) => {
   const [readMore, setReadMore] = useState(false);
 
   const coinDescription = parse(
-    readMore ? coin.description.en : coin.description.en.slice(0, 350),
+    readMore ? coin?.description?.en : coin?.description?.en?.slice(0, 350),
     {
       replace: (domNode) => {
-        if (domNode instanceof Element && domNode.name === 'a') {
+        if (domNode instanceof Element && domNode.name === "a") {
           const props = attributesToProps(domNode?.attribs);
           const text = (domNode.children[0] as TextType).data;
           return (
